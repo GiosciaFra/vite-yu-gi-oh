12c4f92690b548f1b94ba366bcf2eff3ad3e6205
giosciafra <francescogioscia8@gmail.com>
2024-03-04 16:46:50 +0100

    chiamata API correttamente e visualizzazione a schermo delll'array

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
-<script setup>
+<script>
 
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            cards: [],
+        }
+    },
+
+    created() {
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0').then(res => {
+
+            this.cards = res.data.data
+        })
+    },
+
+    mounted() {
+        
+    },
+    
+    components: {
+        
+    },
+    
+    methods: {
+        
+    },
+    
+}
 </script>
 
-<template></template>
+<template>
+    <ul>
+        <li v-for="currentCard in cards">
+    {{ currentCard.name }}
+        </li>
+    </ul>
+</template>
 
 <style lang="scss"></style>
